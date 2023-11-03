5685c49cf4892738ca526e8823bb7b3b9aa9dfbf
Mehmet Sukru Kavak <mehmetsukru.kavak07@gmail.com>
2023-11-03 13:26:16 +0300

    hide turtles, show turtle randomly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import turtle
-
+import random
 FONT = ('Arial', 30, 'normal')
 screen = turtle.Screen()
 
@@ -7,6 +7,9 @@ screen.bgcolor("Light Blue")
 screen.title("Catch The Turtle")
 
 grid_size = 10
+
+#turtle list
+turtle_list = []
 #score turtle
 def setup_score_turtle():
     score_turtle = turtle.Turtle()
@@ -26,14 +29,29 @@ def make_turtle(x, y):
     t.shapesize(2,2)
 
     t.goto(x * grid_size ,y * grid_size)
+    turtle_list.append(t)
 
-setup_score_turtle()
+def hide_turtles():
+    for t in turtle_list:
+        t.hideturtle()
+
+def show_turtle_randomly():
+    random.choice(turtle_list).showturtle();
+
 
 x_coordinates = [-20, -10, 0, 10, 20]
 y_coordinates = [20, 10, 0, -10]
+def setup_turtles():
+    for x in x_coordinates:
+        for y in y_coordinates:
+            make_turtle(x,y)
 
-for x in x_coordinates:
-    for y in y_coordinates:
-        make_turtle(x,y)
+turtle.tracer(0)
+
+setup_score_turtle()
+setup_turtles()
+hide_turtles()
+show_turtle_randomly()
+turtle.tracer(1)
 
 turtle.mainloop()
