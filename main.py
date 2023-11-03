e6f4acb52ef019b7194ecda07018da48cb5f4dca
Mehmet Sukru Kavak <mehmetsukru.kavak07@gmail.com>
2023-11-03 13:17:45 +0300

    set turtles

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ screen = turtle.Screen()
 screen.bgcolor("Light Blue")
 screen.title("Catch The Turtle")
 
-
+grid_size = 10
 #score turtle
 def setup_score_turtle():
     score_turtle = turtle.Turtle()
@@ -19,6 +19,21 @@ def setup_score_turtle():
     score_turtle.setpos(0,  y)
     score_turtle.write(arg="Score: 0", move=False, align="center", font=FONT)
 
+def make_turtle(x, y):
+    t = turtle.Turtle()
+    t.penup()
+    t.shape("turtle")
+    t.shapesize(2,2)
+
+    t.goto(x * grid_size ,y * grid_size)
+
 setup_score_turtle()
+
+x_coordinates = [-20, -10, 0, 10, 20]
+y_coordinates = [20, 10, 0, -10]
+
+for x in x_coordinates:
+    for y in y_coordinates:
+        make_turtle(x,y)
 
 turtle.mainloop()
